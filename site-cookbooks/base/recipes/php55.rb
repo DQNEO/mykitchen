7a46d1f44ac2530b2825147ff3b891eb6e7a615b
DQNEO <dqneoo@gmail.com>
2014-03-20 22:58:43 +0900

    add php packages

--- site-cookbooks/base/recipes/php55.rb
+++ site-cookbooks/base/recipes/php55.rb
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 # install PHP by remi-php55
 # これでhttpdも一緒にインストールされる。
-%w{php php-pear php-mbstring php-xml php-devel php-gd}.each do |pkg|
+%w{
+php php-pear php-mbstring php-xml php-devel php-gd
+php-pgsql php-pdo
+php-pecl-imagick php-pecl-mongo php-pecl-memcache php-pecl-apc php-mcrypt
+}.each do |pkg|
     package pkg do
         options "--enablerepo=remi-php55,remi"
         action :install
