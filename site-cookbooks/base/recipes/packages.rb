feb93e85657a735c00c33f775ee3a24b840613b9
DQNEO <dqneoo@gmail.com>
2014-03-21 13:39:57 +0900

    add subversion package

--- site-cookbooks/base/recipes/packages.rb
+++ site-cookbooks/base/recipes/packages.rb
@@ -1,5 +1,5 @@
 include_recipe 'build-essential'
-%w{man screen tree telnet nkf nano}.each do |pkg|
+%w{man screen tree telnet nkf nano subversion}.each do |pkg|
   package pkg do
     action :install
   end
