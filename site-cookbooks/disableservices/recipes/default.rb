17b3f98c91d7cb509f61cba6df0ec96afc3c3529
DQNEO <dqneoo@gmail.com>
2014-04-26 12:43:13 +0900

    fix typo

--- site-cookbooks/disableservices/recipes/default.rb
+++ site-cookbooks/disableservices/recipes/default.rb
@@ -4,8 +4,8 @@
 # 下記ページを参考にした。
 # http://ry.tl/daemon.html
 # http://shogogg.hatenablog.jp/entry/20091215/1260879331
-# http://tanaka.sakura.ad.jp/archives/001065.html 
-node['dislableservices'].each do |name|
+# http://tanaka.sakura.ad.jp/archives/001065.html
+node['disableservices'].each do |name|
   service name do
     action [ :stop, :disable ]
     only_if 'echo /etc/rc3.d/S* | grep ' + name
