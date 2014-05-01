a25f28dcb9fd473a3144678f731cf1b6207c881f
DQNEO <dqneoo@gmail.com>
2014-05-01 16:49:46 +0900

    refactor

--- site-cookbooks/base/recipes/packages.rb
+++ site-cookbooks/base/recipes/packages.rb
@@ -1,6 +1,8 @@
 
 
-%w{man screen tree telnet nkf nano subversion strace gdb man-pages}.each do |pkg|
+%w{
+man screen tree telnet nkf nano subversion strace gdb man-pages
+}.each do |pkg|
   package pkg do
     action :install
   end
